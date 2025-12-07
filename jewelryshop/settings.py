36c054a7b4c3b59dbdc625efea8a715e6dfb879a
eyern <eyern254@gmail.com>
2025-12-07 04:56:16 +0300

    company logo

--- jewelryshop/settings.py
+++ jewelryshop/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = 'django-insecure-3%y3laftm62q0zaj+s7#p-xqq9(&#q+)s8)p-&#&bz*0$!xu$0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['sheegiftshop.com', 'https://sheegiftshop.com/','shee-production.up.railway.app','https://shee-production.up.railway.app']
+ALLOWED_HOSTS = ['127.0.0.1', 'sheegiftshop.com', 'https://sheegiftshop.com/','shee-production.up.railway.app','https://shee-production.up.railway.app']
 
 CSRF_TRUSTED_ORIGINS = ['https://sheegiftshop.com','https://shee-production.up.railway.app']
 
